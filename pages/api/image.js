2397ca2e304036f6ccae59a1ca2ad5feb9a5213b
Natan Manor <natanmanor@gmail.com>
2023-02-16 17:02:39 +0200

    Switch fetch with Axios call

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,7 +1,10 @@
+import axios from 'axios';
+import { Buffer } from 'buffer';
+
 export default async function handler(req, res) {
-  const imageUrl = req.query.url; // URL of the image to fetch
-  const response = await fetch(imageUrl);
-  const imageBuffer = await response.buffer();
-  res.setHeader('Content-Type', 'image/jpeg'); // Set the content type to the appropriate image format
+  const imageUrl = req.query.url;
+  const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+  const imageBuffer = Buffer.from(response.data, 'binary');
+  res.setHeader('Content-Type', 'image/jpeg');
   res.send(imageBuffer);
 }
