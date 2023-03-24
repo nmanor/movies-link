0ab2638ffd769d0f07af721232c025d3ac051eb3
Natan Manor <natanmanor@gmail.com>
2023-03-24 17:56:53 +0300

    Redesign the navigation bar

--- utils/dates.js
+++ utils/dates.js
@@ -7,3 +7,25 @@ export default function tmdbDateToJsDate(date) {
   const [year, month, day] = date.split('-').map(Number);
   return new Date(year, month - 1, day);
 }
+
+/**
+ * Returns a greeting based on the current time of day.
+ * @param userName {string|null} The username to greet [optional].
+ * @returns {string} A greeting based on the current time of day.
+ */
+export function greetByTime(userName = null) {
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  const name = userName ? `, ${userName}!` : '!';
+
+  if (currentHour < 5) {
+    return `Good night${name}`;
+  } if (currentHour < 12) {
+    return `Good morning${name}`;
+  } if (currentHour < 18) {
+    return `Good afternoon${name}`;
+  } if (currentHour < 22) {
+    return `Good evening${name}`;
+  }
+  return `Good night${name}`;
+}
