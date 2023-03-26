d94e530da5dfbbc101004fcd5bc588c5414aa2bb
Natan Manor <natanmanor@gmail.com>
2023-03-26 16:55:23 +0300

    Add home page background and media icons

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,14 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import axios, { HttpStatusCode } from 'axios';
+import { PropTypes } from 'prop-types';
 import styles from '../styles/Home.module.css';
 import getServerSidePropsLoginMiddleware from '../middlware/getServerSidePropsLoginMiddleware';
 import redirectToPage from '../utils/redirectToPage';
 import { greetByTime } from '../utils/dates';
 import BlankSearchSVGComponent from '../components/shared/svg/Search/BlankSearchSVGComponent';
+import CameraSVGComponent from '../components/shared/svg/CameraSVGComponent';
+import TvSVGComponent from '../components/shared/svg/TvSVGComponent';
 
 export default function Home({ user, trending }) {
   return (
@@ -49,7 +52,12 @@ export default function Home({ user, trending }) {
                 height={0}
                 priority={i < 3}
               />
-              <h3>{title}</h3>
+              <div className={styles.mediaTitle}>
+                {id.startsWith('m')
+                  ? <CameraSVGComponent />
+                  : <TvSVGComponent />}
+                <h3>{title}</h3>
+              </div>
             </Link>
           ))}
         </div>
@@ -57,6 +65,16 @@ export default function Home({ user, trending }) {
     </>
   );
 }
+
+Home.propTypes = {
+  user: PropTypes.instanceOf(Object),
+  trending: PropTypes.arrayOf(Object),
+};
+
+Home.defaultProps = {
+  user: {},
+  trending: [],
+};
 
 export const getServerSideProps = getServerSidePropsLoginMiddleware(async (context) => {
   try {
