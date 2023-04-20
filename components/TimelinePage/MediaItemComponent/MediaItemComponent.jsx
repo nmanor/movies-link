607c27eda599b391e96e03cb76618d6ffe3147de
Natan Manor <natanmanor@gmail.com>
2023-04-20 16:09:09 +0300

    Redesign timeline page

--- components/TimelinePage/MediaItemComponent/MediaItemComponent.jsx
+++ components/TimelinePage/MediaItemComponent/MediaItemComponent.jsx
@@ -1,77 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
-import { Parallax, ParallaxBanner } from 'react-scroll-parallax';
 import Link from 'next/link';
-import MediaType from '../../../utils/enums';
+import Image from 'next/image';
 import styles from './MediaItemComponent.module.css';
 import { groupNameToAcronyms } from '../../../utils/utils';
-import TvSVGComponent from '../../shared/svg/TvSVGComponent';
-import CameraSVGComponent from '../../shared/svg/CameraSVGComponent';
-
-const cx = classNames.bind(styles);
+import NextArrowSVGComponent from '../../shared/svg/NextArrowSVGComponent';
 
 export default function MediaItemComponent({
-  showDay,
-  showMonth,
-  showYear,
+  priority,
+  htmlId,
   media: {
     name, posterUrl, date, id, mediaType, groupName, groupColor,
   },
 }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.sidebar}>
-        <div className={styles.date}>
-          <p className={cx({ [styles.show]: showYear })}>
-            {date ? date.getFullYear() : '.'}
-          </p>
-          <p className={cx({ [styles.show]: showMonth })}>
-            {date ? date.toLocaleString('en', { month: 'short' }).toUpperCase() : '.'}
-          </p>
-          <p className={cx({ [styles.show]: showDay })}>
-            {String(date ? date.getDate() : 0).padStart(2, '0')}
-          </p>
-        </div>
+    <div className={styles.container} id={htmlId}>
+      <div className={styles.side}>
+        <div className={styles.circle} />
         <div className={styles.line} />
       </div>
-      <Parallax scale={[0.9, 1]} className={styles.parallax}>
-        <Link className={styles.movieCard} href={`/media/${id}`}>
-          <ParallaxBanner
-            layers={[{ image: posterUrl, speed: 2 }]}
-            className={styles.movieImage}
-          />
-          <div className={styles.blurredEdge} />
-          <div className={styles.movieDetails}>
-            {mediaType === MediaType.Movie
-              ? <CameraSVGComponent className={styles.svgIcon} />
-              : <TvSVGComponent className={styles.svgIcon} />}
-            <h3>{name}</h3>
-          </div>
-          {groupName && groupColor && (
-          <p
-            className={styles.groupIcon}
-            style={{ backgroundColor: groupColor }}
-          >
-            {groupNameToAcronyms(groupName)}
+      <Link
+        href={`/media/${id}`}
+        className={styles.data}
+        type="button"
+      >
+        <Image
+          priority={priority}
+          src={posterUrl}
+          alt={`Image of ${name}`}
+          width={120}
+          height={180}
+          className={styles.mediaImage}
+        />
+        <div className={styles.movieDetails}>
+          {date && (
+          <p className={styles.date}>
+            {date.toLocaleDateString('he-IL')}
+            {groupName && groupColor && (
+            <span className={styles.groupIcon} style={{ backgroundColor: groupColor }}>
+              {groupNameToAcronyms(groupName)}
+            </span>
+            )}
           </p>
           )}
-        </Link>
-      </Parallax>
+          <p className={styles.mediaTitle}>{name}</p>
+          <p className={styles.mediaType}>{mediaType}</p>
+        </div>
+        <NextArrowSVGComponent className={styles.svgIcon} />
+      </Link>
     </div>
   );
 }
 
 MediaItemComponent.propTypes = {
-  showDay: PropTypes.bool,
-  showMonth: PropTypes.bool,
-  showYear: PropTypes.bool,
+  priority: PropTypes.bool,
+  htmlId: PropTypes.string,
   media: PropTypes.instanceOf(Object),
 };
 
 MediaItemComponent.defaultProps = {
-  showDay: false,
-  showMonth: false,
-  showYear: false,
+  priority: false,
+  htmlId: undefined,
   media: {},
 };
--- components/TimelinePage/YearsPickerComponent/YearsPickerComponent.jsx
+++ components/TimelinePage/YearsPickerComponent/YearsPickerComponent.jsx
@@ -1,19 +1,11 @@
-import React, { useRef, Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+import PropTypes, { number, string } from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './YearsPickerComponent.module.css';
 
 const cx = classNames.bind(styles);
 
-export default function YearsPickerComponent({ years, currentYear, onYearClick }) {
-  const refs = Object.fromEntries(years.map((year) => [year, useRef(null)]));
-
-  // useEffect(() => refs[currentYear].current.scrollIntoView({
-  //   behavior: 'smooth',
-  //   block: 'center',
-  //   inline: 'center',
-  // }), [currentYear]);
-
+export default function YearsPickerComponent({ years, onClick, currentYear }) {
   return (
     <div className={styles.container}>
       {years
@@ -21,8 +13,7 @@ export default function YearsPickerComponent({ years, currentYear, onYearClick }
         .map((year, i, arr) => (
           <Fragment key={year}>
             <button
-              ref={refs[year]}
-              onClick={() => onYearClick(year)}
+              onClick={() => onClick(year)}
               type="button"
               className={cx(styles.year, { [styles.selectedYear]: currentYear === year })}
             >
@@ -37,12 +28,12 @@ export default function YearsPickerComponent({ years, currentYear, onYearClick }
 
 YearsPickerComponent.propTypes = {
   years: PropTypes.arrayOf(Number),
-  currentYear: PropTypes.number,
-  onYearClick: PropTypes.func,
+  onClick: PropTypes.func,
+  currentYear: PropTypes.oneOfType(string, number),
 };
 
 YearsPickerComponent.defaultProps = {
   years: [2018, 2019, 2020, 2021, 2022, 2023],
-  currentYear: 2020,
-  onYearClick: () => {},
+  onClick: () => {},
+  currentYear: 'unknown',
 };
