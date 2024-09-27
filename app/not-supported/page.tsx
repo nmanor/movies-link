ffdb28e071d9935e2ea49640cd418b555bf4542e
Natan Manor <natanmanor@gmail.com>
2024-09-27 15:17:56 +0300

    Migrate not-supported into app router

---
+++ app/not-supported/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React from 'react';
+import Lottie from 'lottie-react';
+import styles from './not-supported.module.css';
+import animationData from '@/public/lotties/mobile.json';
+
+export default function NotSupported() {
+    return (
+        <>
+            <title>Unsupported device</title>
+            <div className={styles.container}>
+                <Lottie animationData={animationData} className={styles.lottie} loop={0} />
+                <div>
+                    <h1>Sorry,</h1>
+                    <p>We currently only support cell phones...</p>
+                </div>
+            </div>
+        </>
+    );
+}
